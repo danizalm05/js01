5370cbe6696dc06eea90a68a23dd8912b705a289
danizalm05 <danizalm05@gmail.com>
2022-07-25 17:34:30 +0300

    31328

--- nodejs/codingTrain/index.js
+++ nodejs/codingTrain/index.js
@@ -1,3 +1,6 @@
+ //Working with Data and APIs in JavaScript  Coding Train
+ 
+ //https://www.youtube.com/playlist?list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X
  //https://www.youtube.com/watch?v=wxbQP1LMZsw&list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X&index=9
  //14:00
  
@@ -5,7 +8,7 @@
  
  const app  = express()
  
- app.listen(3000,()=> console.log('listing at 300' ))
- app.use(express.static('public'))
+ app.listen(3000,()=> console.log('listing at http//localhost:3000' ))
+ app.use(express.static('public'))// public is the directory than contains the 'index.html'
  
  
