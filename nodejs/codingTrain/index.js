87723bbbf59b35665507423b611d5c7a9b24eed3
danizalm05 <danizalm05@gmail.com>
2022-08-11 15:03:21 +0300

    14087

--- nodejs/codingTrain/index.js
+++ nodejs/codingTrain/index.js
@@ -2,13 +2,13 @@
  
  //https://www.youtube.com/playlist?list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X
  //https://www.youtube.com/watch?v=wxbQP1LMZsw&list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X&index=9
- //14:00
+ //15:00
  
  const express = require('express')
  
- const app  = express()
+ const app  = express() ;  
  
  app.listen(3000,()=> console.log('listing at http//localhost:3000' ))
- app.use(express.static('public'))// public is the directory than contains the 'index.html'
+ app.use(express.static('module2'))// public is the directory than contains the 'index.html'
  
  
