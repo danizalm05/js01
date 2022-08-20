dbfb45ff6c8835f9004197e354b4896b1a4746ed
danizalm05 <danizalm05@gmail.com>
2022-08-20 16:33:06 +0300

    15102

--- nodejs/codingTrain/queryDB.js
+++ nodejs/codingTrain/queryDB.js
@@ -3,7 +3,7 @@
 // npm instal  nedb
  //https://www.youtube.com/watch?v=xVYa20DCUv0&list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X&index=12
  
- //To start  run:    'nodemon saving2DB.js '
+ //To start  run:    'nodemon queryDB.js '
 //https://github.com/CodingTrain/Intro-to-Data-APIs-JS/blob/source/module2/04_saving_to_database/index.js
  
 //NeDB: https://github.com/louischatriot/nedb
@@ -11,18 +11,30 @@
 //https://expressjs.com/
  //https://expressjs.com/en/guide/routing.html
  //08:20
-const express = require('express');
-const Datastore = require('nedb');
+
+  
+const express = require("express");
+const Datastore = require("nedb");
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('module2'));
-app.use(express.json({ limit: '1mb' }));
+app.listen(3000, () => console.log("listening at 3000"));
+app.use(express.static("module2/05_querying_database"));//location of the index
+app.use(express.json({ limit: "1mb" }));
 
-const database = new Datastore('database.db');
+const database = new Datastore("database.db");
 database.loadDatabase();
 
-app.post('/api_test', (request, response) => {
+app.get("/api", (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.send(data);
+  });
+});
+
+app.post("/api", (request, response) => {
   const data = request.body;
   const timestamp = Date.now();
   data.timestamp = timestamp;
