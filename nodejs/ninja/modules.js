c09704bace4ec0e2f49ef4efacdb8312e41e8a08
danizalm05 <danizalm05@gmail.com>
2022-09-11 16:42:04 +0300

    30904

---
+++ nodejs/ninja/modules.js
@@ -0,0 +1,19 @@
+//https://github.com/iamshaunjp/node-crash-course/blob/lesson-2/modules.js
+//https://www.youtube.com/watch?v=OIBIXYLJjsI&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=3
+//10:00
+const people = require('./people');
+
+console.log("[ ",people," ]");
+
+// const data = require('./people');
+
+// console.log(data.people, data.ages);
+/*
+const { people, ages } = require('./people');
+
+console.log(people, ages);
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
+*/
---
+++ nodejs/ninja/people.js
@@ -0,0 +1,14 @@
+//https://github.com/iamshaunjp/node-crash-course/blob/lesson-2/people.js
+const peopl = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages = [20, 25, 30, 35];
+
+// console.log(people);
+
+// module.exports = people;
+/*
+module.exports = {
+  peopl,
+  ages,
+}*/
+
+console.log("- ",peopl," -");
