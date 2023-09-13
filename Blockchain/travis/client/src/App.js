92436a731e473d3a05f1391f6cdaae80f39c0b55
Richm <ric5hme45@yahoo.com>
2023-09-13 11:48:51 +0300

    6942

--- Blockchain/travis/client/src/App.js
+++ Blockchain/travis/client/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+ 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="container">
+      <div className="row mt-5">
+
+        <div className="col">
+            <p>Contract Balance:   ETH</p>
+        </div>
+
+        <div className="col">
+          <div className="mb-3">
+            <h4>Deposit ETH</h4>
+            <form  >
+              <div className="mb-3">
+                <input type="number" className="form-control" placeholder="0"  value = "test"   />
+              </div>
+              <button type="submit" className="btn btn-success">Deposit</button>
+            </form>
+
+            <h4 className="mt-3">Change Greeting</h4>
+            <form  >
+              <div className="mb-3">
+                <input type="text" className="form-control" placeholder=""     />
+              </div>
+              <button type="submit" className="btn btn-dark">Change</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  );;
 }
 
 export default App;
